33dd66102931de1bd16b554fbd11c20034af8d41
wandererxp <90262552+wandererxp@users.noreply.github.com>
2022-02-13 22:10:43 +0530

    Update download.c

--- download.c
+++ download.c
@@ -3,6 +3,8 @@
  *
  *  Copyleft 2022   Wanderer XP. 
  *
+ *  Fair Warning: I am not a pro at C. So this code might offend you.
+ *
  *  This program is free software: you can redistribute it and/or modify it under the terms of the 
  *  GNU General Public License as published by the Free Software Foundation, either version 3 
  *  of the License, or (at your option) any later version.
@@ -17,6 +19,12 @@
 
 #include <stdio.h>
 #include <curl/curl.h>
+
+int cleanup_dwnld() {
+    fclose(fp);
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+}
 
 int main(int argc, char **argv) {
     CURL *curl;
@@ -36,15 +44,20 @@ int main(int argc, char **argv) {
     if (result == CURLE_OK)
     {
         printf("Download Successful. File saved as %s.\n", argv[2]);
+        
+        fclose(fp);
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
         return 0;
     }
     else
     {
         printf("Error: %s\n", curl_easy_strerror(result));
+        fclose(fp);
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
         return 1;
     }
     
-    fclose(fp);
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
+
 }
